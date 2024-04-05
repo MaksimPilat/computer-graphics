110f81eee06b231cf60136be53f29b4be9cbe331
MaksimPilat <pilatmd@outlook.com>
2024-04-05 11:32:57 +0300

    modified:   src/Canvas3D.ts

--- src/Canvas3D.ts
+++ src/Canvas3D.ts
@@ -4,6 +4,12 @@ export type Canvas3DOptions = {
   root: HTMLElement;
 };
 
+export type Point3D = {
+  x: number;
+  y: number;
+  z: number;
+};
+
 export class Canvas3D {
   private scene = new THREE.Scene();
   private camera = new THREE.PerspectiveCamera(
@@ -14,20 +20,18 @@ export class Canvas3D {
   );
   private renderer = new THREE.WebGLRenderer();
   private resizeObserver: ResizeObserver;
-
   private animationFrameId: number | null = null;
   private isDragging = false;
-
   private mousePosition = {
     previous: { x: 0, y: 0 },
     current: { x: 0, y: 0 },
   };
+  private currentShape: string | null = null;
 
   constructor({ root }: Canvas3DOptions) {
     this.resizeObserver = new ResizeObserver((entries) => {
       const { target } = entries[0];
       const { width, height } = target.getBoundingClientRect();
-
       this.renderer.setSize(width, height);
       this.clear();
     });
@@ -35,6 +39,9 @@ export class Canvas3D {
     this.resizeObserver.observe(root);
 
     this.scene.background = new THREE.Color('#ffffff');
+
+    this.camera.position.z = 2;
+    this.camera.userData = { theta: 0, phi: 0 };
 
     root.appendChild(this.renderer.domElement);
 
@@ -52,6 +59,8 @@ export class Canvas3D {
   drawCube() {
     this.clear();
 
+    this.currentShape = 'cube';
+
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const edges = new THREE.EdgesGeometry(geometry);
     const material = new THREE.LineBasicMaterial({ color: 0x000000 });
@@ -60,49 +69,7 @@ export class Canvas3D {
 
     this.scene.add(line);
 
-    this.camera.position.z = 3;
-
-    const animateCube = () => {
-      this.animationFrameId = requestAnimationFrame(() => animateCube());
-
-      const line = this.scene.getObjectByName(
-        'cube'
-      ) as THREE.Object3D<THREE.Object3DEventMap>;
-
-      if (!this.isDragging) {
-        const deltaMove = {
-          x: 0.5,
-          y: 0.3,
-          z: 0.4,
-        };
-
-        const axis = new THREE.Vector3(deltaMove.y, deltaMove.x, deltaMove.z);
-        const angle = axis.length() * 0.01;
-
-        line.rotateOnAxis(axis.normalize(), angle);
-      } else {
-        const deltaMove = {
-          x: this.mousePosition.previous.x - this.mousePosition.current.x,
-          y: this.mousePosition.previous.y - this.mousePosition.current.y,
-        };
-
-        if (deltaMove.x || deltaMove.y) {
-          const axis = new THREE.Vector3(deltaMove.y, deltaMove.x, 0);
-          const angle = axis.length() * 0.01;
-
-          line.rotateOnWorldAxis(axis.normalize(), angle);
-        }
-
-        this.mousePosition.previous = {
-          x: this.mousePosition.current.x,
-          y: this.mousePosition.current.y,
-        };
-      }
-
-      this.renderer.render(this.scene, this.camera);
-    };
-
-    animateCube();
+    this.animateCamera();
   }
 
   drawPyramid() {
@@ -220,12 +187,16 @@ export class Canvas3D {
   }
 
   clear() {
+    this.currentShape = null;
+
     if (this.animationFrameId !== null) {
       cancelAnimationFrame(this.animationFrameId);
       this.animationFrameId = null;
     }
+
     this.scene.clear();
     this.renderer.clear();
+    this.camera.clear();
   }
 
   disable() {
@@ -252,11 +223,66 @@ export class Canvas3D {
   }
 
   private onMouseMove(event: MouseEvent) {
-    if (this.isDragging) {
-      this.mousePosition.current = {
+    if (this.isDragging && this.currentShape) {
+      const deltaMove = {
+        x: event.clientX - this.mousePosition.previous.x,
+        y: event.clientY - this.mousePosition.previous.y,
+        z: 0,
+      };
+
+      this.rotateShape(this.currentShape, deltaMove);
+
+      this.mousePosition.previous = {
         x: event.clientX,
         y: event.clientY,
       };
     }
+  }
+
+  private rotateShape(shape: string, deltaMove: Point3D) {
+    const object = this.scene.getObjectByName(
+      shape
+    ) as THREE.Object3D<THREE.Object3DEventMap>;
+
+    const axis = new THREE.Vector3(deltaMove.y, deltaMove.x, 0);
+    const angle = axis.length() * 0.01;
+    object.rotateOnWorldAxis(axis.normalize(), angle);
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  private rotateCamera(deltaMove: Point3D) {
+    const center = new THREE.Vector3(0, 0, 0);
+
+    const radius = 2;
+    const frequency = 0.5;
+    const theta = -deltaMove.x * 0.01;
+    const phi = -deltaMove.y * 0.01;
+
+    this.camera.userData.theta += theta;
+    this.camera.userData.phi += phi;
+
+    const x = radius * Math.sin(this.camera.userData.theta);
+    const y =
+      radius *
+      Math.sin(this.camera.userData.phi) *
+      Math.cos(frequency * this.camera.userData.theta);
+    const z = radius * Math.cos(this.camera.userData.phi);
+    const newPosition = new THREE.Vector3(x, y, z);
+
+    this.camera.position.copy(newPosition);
+    this.camera.lookAt(center);
+  }
+
+  animateCamera() {
+    this.animationFrameId = requestAnimationFrame(
+      this.animateCamera.bind(this)
+    );
+
+    if (!this.isDragging) {
+      this.rotateCamera({ x: 0.5, y: 0.3, z: 0 });
+    }
+
+    this.renderer.render(this.scene, this.camera);
   }
 }
